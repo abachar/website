5632794c06a8fbeebcd509990db10b8c757bb7e2
Abdelhakim Bachar <a.bachar@hotmail.fr>
2015-04-01 11:15:25 +0200

    Update app.js
    Add max-age header to cache static

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express["static"](path.join(__dirname, 'public')));
-app.use('/bower_components', express["static"](__dirname + '/../bower_components'));
+app.use(express["static"](path.join(__dirname, 'public'), {maxAge: 86400}));
+app.use('/bower_components', express["static"](__dirname + '/../bower_components', {maxAge: 86400}));
 
 // Routing
 require('./routes/home')(app);
