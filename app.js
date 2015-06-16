e49bebed65bcbbe5f6dc542fc64c12c2d8e31235
Abdelhakim Bachar <a.bachar@hotmail.fr>
2015-06-16 14:14:46 +0200

    Include the new proxy route

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ require('./routes/profile')(app);
 require('./routes/projects')(app);
 require('./routes/blog')(app);
 require('./routes/contact')(app);
+require('./routes/proxy')(app);
 
 // catch 404 and forward to error handler
 app.use(function (request, response, next) {
